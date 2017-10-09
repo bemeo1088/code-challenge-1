b5d02415ea142115c2c4bc409cfec144e76165fc
Hanna Nguyen <ha.nguyenthidan@gmail.com>
2017-10-09 14:12:35 -0500

    Inside the <div> element you created in the previous step, create a <p> element that shows how many times you have clicked the button from the first step.

--- client.js
+++ client.js
@@ -6,6 +6,12 @@ function readyNow() {
   $('#generateBtn').on('click', generateBtnClicked);
 }
 
+var clickCounter = 0;
+
 function generateBtnClicked() {
-    $('.main').append('<div></div>');
+
+  $('.generateBtn').each(function(){
+    clickCounter += 1;
+  });
+  $('.main').append('<div><p>' + clickCounter + '</p></div>');
 }
